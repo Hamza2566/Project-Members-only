31bc11b2beac735ea24827efd9d5fb7dde25776e
Hamza <xsharif2551@gmail.com>
2025-08-27 15:34:13 +0300

    fixing for deployment #5

--- server/config/db.js
+++ server/config/db.js
@@ -5,8 +5,8 @@ const { Pool } = pkg;
 dotenv.config();
 
 const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }          
+  connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 export default db;
